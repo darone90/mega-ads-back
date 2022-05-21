3c9c3cd62a47ebb35b77f1613f4c0afc1bcc13e3
Dariusz Pilecki <pileckidariusz90@gmail.com>
2022-05-21 19:00:11 +0200

    application is ready to test, preparing for new functions

--- routers/ad.router.ts
+++ routers/ad.router.ts
@@ -6,13 +6,14 @@ export const adRouter = Router()
 adRouter
 
     .get('/search/:name?', async (req, res) => {
-
+        console.log(req.params.name)
         const ads = await AdRecord.findAll(req.params.name ?? '');
 
         res.json(ads)
     })
 
     .get('/:id', async (req, res) => {
+        console.log(req.params.id)
         const ad = await AdRecord.getOne(req.params.id);
 
         res.json(ad);
